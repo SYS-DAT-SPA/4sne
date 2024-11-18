8b04d516fbd8d0c118c71321a90dbfe5eb15b3d4
MantoVincenzo <vincenzo.manto@sys-datgroup.com>
2024-11-18 12:51:54 +0100

    fix: update base URL in docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config = {
   title: docusaurusData.title || "My Site",
   tagline: docusaurusData.tagline || "Dinosaurs are cool",
   url: docusaurusData.url,
-  baseUrl: "/",
+  baseUrl: "/4sne/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/logo.png",
